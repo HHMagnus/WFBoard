7676ed777e864034ca15f1ddd04fb338e91297a4
= <8402802+HHMagnus@users.noreply.github.com>
2019-08-18 22:31:12 +0200

    start of scoring

--- scripts/calculateRankings.js
+++ scripts/calculateRankings.js
@@ -1,3 +1,24 @@
+
+function rankLeaderboard(json){
+    return (leaderboard) => {
+        
+    };
+}
+
 export default (json) => {
-    
+    let leaderboards = [...json.categories.flatMap(category => category.leaderboards), ...json.levels.flatMap(level => level.leaderboards)];
+
+    leaderboards.forEach(rankLeaderboard(json))
+
+    console.log(leaderboards);
+}
+
+function score (place, total_runs){
+    if (place == 1)
+        return 3;
+    if (place == 2)
+        return 2;
+    if (place == 3)
+        return 1;
+    return 0;
 }
