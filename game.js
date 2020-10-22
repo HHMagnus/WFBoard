09ec5efdb6c28a226b0c0c37a156f13a4c3c2a6c
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2020-10-22 23:23:48 +0200

    Levels and Full Game UIs

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import {getJsonFromUrl } from '/scripts/parseUrlParams.js';
+import { getJsonFromUrl } from '/scripts/parseUrlParams.js';
 import fetchOptions from '/scripts/fetchOptions.js';
 import formatGameJson from '/scripts/formatGameJson.js';
 import { generateLeaderboards, fetchLeaderboards } from '/scripts/leaderboards.js';
@@ -9,7 +9,7 @@ import active_tabs from '/scripts/active_tabs.js';
 
 async function start() {
     let url_params = getJsonFromUrl();
-    let gameid =  url_params.game;
+    let gameid = url_params.game;
 
     let response = await fetch("https://www.speedrun.com/api/v1/games/" + gameid + "?embed=levels.variables,levels.categories,categories.variables", fetchOptions);
     let json = await response.json();
@@ -23,7 +23,7 @@ async function start() {
 
     json.players = extractPlayers(json);
 
-    let {playerRanks, runScores} = calculateRankings(json);
+    let { playerRanks, runScores } = calculateRankings(json);
 
     populateView(json, playerRanks, runScores);
 
--- scripts/populateView.js
+++ scripts/populateView.js
@@ -1,17 +1,38 @@
 const personal_tbody = document.querySelector("#personal_tbody");
 const playerViewTemplate = document.querySelector("#playerView");
 
+const level_tbody = document.querySelector("#level_tbody");
+const levelViewTemplate = document.querySelector("#levelView");
+
+const full_game_tbody = document.querySelector("#full_game_tbody");
+const fullGameViewTemplate = document.querySelector("#fullGameView");
+
+const leaderboardOverlayViewTemplate = document.querySelector("#leaderboardOverlayView");
+const leaderboardScoreViewTemplate = document.querySelector("#leaderboardScoreView");
+
+function playerLinkView(player_id, players) {
+    let player = players.find (player => player.id == player_id);
+    let player_name = "Unknown";
+    let player_profile = "#";
+    if(player.rel == "guest") {
+        player_name = player.name;
+        player_profile = player.uri;
+    } else {
+        player_name = player.names.international;
+        player_profile = player.weblink;
+    }
+
+    let player_element = document.createElement("a");
+    player_element.href = player_profile;
+    player_element.innerText = player_name;
+    return player_element;
+}
+
 function populatePersonalRankings (json, playerRanks) {
     playerRanks.forEach (rank => {
-        let player = json.players.find (player => player.id == rank.id);
-        let player_name = player.names.international;
-        let player_profile = player.weblink;
-
         let playerView = document.importNode(playerViewTemplate.content, true);
 
-        let player_element = document.createElement("a");
-        player_element.href = player_profile;
-        player_element.innerText = player_name;
+        let player_element = playerLinkView(rank.id, json.players);
         playerView.querySelector(".player_name").appendChild(player_element);
 
         let level_score =  playerView.querySelector(".level_score");
@@ -32,11 +53,82 @@ function populatePersonalRankings (json, playerRanks) {
 
     sorttable.makeSortable(document.querySelector("#personal_table"));
 }
+function populateLevels(json, runScores) {
+    json.levels.forEach(level => {
+        level.leaderboards.forEach(leaderboard => {
+            // Insert run
+            let levelView = document.importNode(levelViewTemplate.content, true);
+            let level_button = document.createElement("a");
+            level_button.addEventListener('click', () => {
+                showLeaderboardOverlay(json, leaderboard, runScores);
+            });
+            level_button.innerText = leaderboard.name;
+            level_button.href = "#" + leaderboard.name;
+
+            let level_name = levelView.querySelector(".level_name");
+            level_name.appendChild(level_button);
+
+            level_tbody.append(levelView);
+        });
+    });
+}
+
+function showLeaderboardOverlay(json, leaderboard, runScores) {
+    // Insert overview
+    let levelOverlayView = document.importNode(leaderboardOverlayViewTemplate.content, true);
+
+    let level_runs = levelOverlayView.querySelector(".level_runs");
+    leaderboard.data.runs.forEach(run => {
+        let levelScoresView = document.importNode(leaderboardScoreViewTemplate.content, true);
+
+        let place = levelScoresView.querySelector(".place");
+        place.innerText = run.place;
+
+        let player_name = levelScoresView.querySelector(".player_name");
+        run.run.players.forEach(player => {
+            let player_element = playerLinkView(player.id, json.players);
+            player_element.style.marginLeft = "8px";
+            player_name.appendChild(player_element)
+        });
+
+        let score = levelScoresView.querySelector(".score");
+        score.innerText = runScores[run.run.id].score;
+
+        level_runs.appendChild(levelScoresView);
+    });
+
+    let visit_level = levelOverlayView.querySelector(".visit_level");
+    visit_level.href = leaderboard.data.weblink;
+
+    document.body.appendChild(levelOverlayView);
+}
+
+function populateFullGame(json, runScores) {
+    json.categories.forEach(category => {
+        category.leaderboards.forEach(leaderboard => {
+            let fullGameView = document.importNode(fullGameViewTemplate.content, true);
+            let full_game_button = document.createElement("a");
+            full_game_button.addEventListener('click', () => {
+                showLeaderboardOverlay(json, leaderboard, runScores);
+            });
+            full_game_button.innerText = leaderboard.name;
+            full_game_button.href = "#" + leaderboard.name;
+
+
+            let full_game_name = fullGameView.querySelector(".name");
+            full_game_name.appendChild(full_game_button);
+
+            full_game_tbody.append(fullGameView);
+        });
+    });
+}
 
 export default (json, playerRanks, runScores) => {
     populatePersonalRankings(json,playerRanks);
+    populateLevels(json, runScores);
+    populateFullGame(json, runScores);
 
     removeLoader();
 }
 
-function removeLoader() { document.querySelector("overlay").style.display = "none"; }
+function removeLoader() { document.querySelector("#loader").style.display = "none"; }
