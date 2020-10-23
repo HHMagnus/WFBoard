ccb126298353cac284a258b37e34da68a901c899
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2020-10-23 12:48:49 +0200

    same game data to localStorage

--- game.js
+++ game.js
@@ -6,8 +6,23 @@ import fetchAll from './scripts/fetchAll.js';
 async function start() {
     let url_params = getJsonFromUrl();
     let gameid = url_params.game;
+    let data = {};
 
-    let { json, playerRanks, runScores } = await fetchAll(gameid, (x, y) => fetch(x, y));
+    try {
+        data = await fetchAll(gameid, (x, y) => fetch(x, y));
+        await store_in_localStorage(gameid, data);
+    } catch (err) {
+        try {
+            console.log("Load from localStorage")
+            let fromLocalStorage = await load_from_localStorage(gameid);
+            date = JSON.parse(fromLocalStorage);
+        } catch (err2) {
+            alert("The API has denied your request because of a rate limit. Please try again later!")
+            return;
+        }
+    }
+
+    let { json, playerRanks, runScores } = data;
 
     document.querySelector("title").innerHTML = json.names.international + " rankings";
 
@@ -18,6 +33,33 @@ async function start() {
     console.log(runScores);
 
     active_tabs();
+}
+
+async function store_in_localStorage(gameid, data) {
+    let dataStr = JSON.stringify(data);
+    const mb = 1000000;
+    let bits = parseInt(dataStr.length / mb)+1;
+
+    await localforage.setItem(gameid +"_bits", bits);
+
+    for(let i = 0; i < bits; i++) {
+        let bit = dataStr.substr(i * mb, mb);
+        await localforage.setItem(gameid + "_bit" + i, bit);
+    }
+}
+
+async function load_from_localStorage(gameid) {
+    let result = "";
+
+    let bits = await localforage.getItem(gameid + "_bits");
+
+    for(let i = 0; i < bits; i++) {
+        console.log("Bit get " + i);
+        let bit = await localforage.getItem(gameid + "_bit" + i);
+        result += bit;
+    }
+
+    return result;
 }
 
 start();
